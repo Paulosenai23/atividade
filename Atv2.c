9b7e015878981bfcc281d188754ae671712daf5c
paulo.senai23.des <paulo.senai23.des@gmail.com>
2023-07-28 11:01:37 -0300

    aula1

---
+++ Atv2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int x;
+	printf("Digite a velocidade do carro em KM/H:");
+	scanf("%d",&x);
+	int y;
+	printf("Digite a distancia ha ser percorrida em KM:");
+	scanf("%d",&y);
+	int z;
+	printf("O tempo que será utilizado para percorrer a distancia descrita eh de: %d", z=y/x); 
+	printf("horas");
+	return 0;
+}
